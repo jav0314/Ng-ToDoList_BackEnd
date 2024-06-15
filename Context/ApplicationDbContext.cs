33fc13c73d627416f31e89e96ecbab0380b15d18
JavierM <144504968+jav0314@users.noreply.github.com>
2024-06-14 22:33:04 -0500

    upload backend

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Model;
+
+namespace ToDoList.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SchoolDb;Trusted_Connection=True;");
+        }
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ Model/Users.cs
@@ -0,0 +1,25 @@
+﻿namespace ToDoList.Model
+{
+    public class Users
+    {
+        public int Codigo { get; set; }
+
+        public string id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string DateBirth { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Nationatily { get; set; }
+    }
+}
